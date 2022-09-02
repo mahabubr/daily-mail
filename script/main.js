c3d6969d46e278f485d4afe083d96d3e1cdd8837
mahabubr <mahabub.r753@gmail.com>
2022-09-02 23:29:21 +0600

    catagory data loded

--- script/main.js
+++ script/main.js
@@ -1,1 +1,16 @@
-console.log("check")
+// Catagory Data Loadded
+const loadCatagoryData = async () => {
+    const url = 'https://openapi.programming-hero.com/api/news/categories'
+    const response = await fetch(url);
+    const data = await response.json();
+    displayCatagoryData(data.data.news_category)
+}
+
+// Show Catagory Data Loadded
+const displayCatagoryData = catagoryList => {
+    catagoryList.forEach(list => {
+        console.log(list)
+    })
+}
+
+loadCatagoryData()
