a09c4a5007beb5d64b2edaa5c219b9ee93d3bea8
mahabubr <mahabub.r753@gmail.com>
2022-09-03 00:03:20 +0600

    show catagory loaded

--- script/main.js
+++ script/main.js
@@ -1,16 +1,36 @@
 // Catagory Data Loadded
 const loadCatagoryData = async () => {
-    const url = 'https://openapi.programming-hero.com/api/news/categories'
-    const response = await fetch(url);
-    const data = await response.json();
-    displayCatagoryData(data.data.news_category)
+    try {
+        const url = 'https://openapi.programming-hero.com/api/news/categories'
+        const response = await fetch(url);
+        const data = await response.json();
+        displayCatagoryData(data.data.news_category)
+    }
+    catch (err) {
+        alert(err)
+    }
 }
 
 // Show Catagory Data Loadded
 const displayCatagoryData = catagoryList => {
+    const displayCatagory = document.getElementById('display-catagory')
     catagoryList.forEach(list => {
-        console.log(list)
+        const { category_name, category_id } = list;
+        const catagoryDiv = document.createElement('div');
+        catagoryDiv.classList.add('font-medium', 'text-lg', 'text-slate-600')
+        catagoryDiv.innerHTML = `
+        <a onclick="displayAllNewsById(${category_id})" href="#">${category_name}</a>
+        `
+        displayCatagory.appendChild(catagoryDiv);
     })
+}
+
+// Shpw All News In a Category
+const displayAllNewsById = async id => {
+    const url = `https://openapi.programming-hero.com/api/news/category/01`
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data.data)
 }
 
 loadCatagoryData()
