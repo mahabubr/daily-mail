ea4caf0d28c0da2df649ef0c86f3facf56985209
mahabubr <mahabub.r753@gmail.com>
2022-09-03 00:18:38 +0600

    card div section parse into js

--- script/main.js
+++ script/main.js
@@ -19,18 +19,34 @@ const displayCatagoryData = catagoryList => {
         const catagoryDiv = document.createElement('div');
         catagoryDiv.classList.add('font-medium', 'text-lg', 'text-slate-600')
         catagoryDiv.innerHTML = `
-        <a onclick="displayAllNewsById(${category_id})" href="#">${category_name}</a>
+        <a onclick="displayAllNewsById('${category_id}')" href="#">${category_name}</a>
         `
         displayCatagory.appendChild(catagoryDiv);
     })
 }
 
-// Shpw All News In a Category
-const displayAllNewsById = async id => {
-    const url = `https://openapi.programming-hero.com/api/news/category/01`
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data.data)
+// Loded All News In a Category
+const displayAllNewsById = async category_id => {
+    try {
+        const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`
+        const response = await fetch(url);
+        const data = await response.json();
+        showAllNewsById(data.data)
+    }
+    catch (err) {
+        alert(err)
+    }
+}
+// Show All News In a Category
+const showAllNewsById = catagoryNews => {
+    const displayCatagoryCard = document.getElementById('display-catagory-card');
+    catagoryNews.forEach(news => {
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('card', 'lg:card-side', 'bg-base-100', 'shadow-xl')
+        cardDiv.innerHTML = `
+        
+        `
+    })
 }
 
 loadCatagoryData()
