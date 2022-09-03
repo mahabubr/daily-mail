d5edcdf370571d0df1780af858111b4b5095b4cc
mahabubr <mahabub.r753@gmail.com>
2022-09-03 18:06:18 +0600

    news marks part complete

--- script/main.js
+++ script/main.js
@@ -23,13 +23,14 @@ const displayCatagoryData = catagoryList => {
         catagoryDiv.classList.add('font-medium', 'text-lg', 'text-slate-600')
         catagoryDiv.innerHTML = `
         <div>  
-        <a class="hover:bg-blue-500 hover:p-2 hover:text-white hover:rounded-xl" onclick="displayAllNewsById('${category_id}')" href="#">${category_name}</a>
+    <a class="hover:bg-slate-400 hover:p-2 hover:text-slate-800 hover:rounded-xl" onclick="displayAllNewsById('${category_id}')" href="#">${category_name}</a>
         </div>
         `
         displayCatagory.appendChild(catagoryDiv);
 
     })
 }
+
 
 // Loded All News In a Category
 const displayAllNewsById = async category_id => {
@@ -49,10 +50,12 @@ const showAllNewsById = catagoryNews => {
     const itemsFounds = document.getElementById('items-found-details');
     itemsFounds.innerText = `${catagoryNews.length}`
 
+    catagoryNews.sort((a, b) => (a.total_view - b.total_view));
+
     const displayCatagoryCard = document.getElementById('display-catagory-card');
     displayCatagoryCard.textContent = '';
-    catagoryNews.forEach(news => {
 
+    catagoryNews.forEach(news => {
 
         const { thumbnail_url, title, details, author, total_view, _id } = news;
         const { name, published_date, img } = author
@@ -81,7 +84,7 @@ const showAllNewsById = catagoryNews => {
                         </div>
                         <div class="flex justify-center items-center">
                             <a href=""><i class="fa-solid fa-eye"></i></a>
-                            <h3 class="ml-2 font-medium">${total_view ? total_view : 'N/A'}</h3>
+                            <h3 class="ml-2 font-medium">${total_view ? total_view : '0'}</h3>
                         </div>
                         <div>
                             <div class="rating">
@@ -89,7 +92,7 @@ const showAllNewsById = catagoryNews => {
                                 <input type="radio" name="rating-1" class="mask mask-star bg-amber-400" />
                                 <input type="radio" name="rating-1" class="mask mask-star bg-amber-400" />
                                 <input type="radio" name="rating-1" class="mask mask-star bg-amber-400" checked />
-                                <input type="radio" name="rating-1" class="mask mask-star" />
+                                <input type="radio" name="rating-1" class="mask mask-star bg-amber-400" />
                             </div>
                         </div>
                         <div>
@@ -104,35 +107,29 @@ const showAllNewsById = catagoryNews => {
     })
 }
 
+// Loaded Modal 
 const modalDataLoaded = async (id) => {
     const url = `https://openapi.programming-hero.com/api/news/${id}`
     const response = await fetch(url);
     const data = await response.json();
     displayModalDataLoaded(data.data[0])
 }
-
+// Show Modal 
 const displayModalDataLoaded = data => {
 
     const { image_url, title, details, others_info } = data
 
     const modalArea = document.getElementById('modal-area')
-    const modalDiv = document.createElement('div')
-
-    modalDiv.innerHTML = `
-                <input type="checkbox" id="my-modal-6" class="modal-toggle" />
-                            <div class="modal modal-bottom sm:modal-middle">
-                            <div class="modal-box">
-                            <h3 class="font-bold text-xl text-red-600 mb-2">${others_info.is_trending ? 'is Tranding' : 'N/A'}</h3>
-                            <figure><img class="w-full" src="${image_url}" alt="Shoes" /></figure>
-                                <h3 class="font-bold text-lg mt-2">${title}</h3>
-                                <p class="py-4">${details}</p>
-                                <div class="modal-action">
-                                <label for="my-modal-6" class="btn">Enjoy More News</label>
-                                </div>
-                            </div>
+    modalArea.innerHTML = `
+    <h3 class="font-bold text-xl text-red-600 mb-2">${others_info.is_trending ? 'is Tranding' : 'N/A'}
+                    </h3>
+                    <figure><img class="w-full" src="${image_url}" alt="Shoes" /></figure>
+                    <h3 class="font-bold text-lg mt-2">${title}</h3>
+                    <p class="py-4">${details}</p>
+                    <div class="modal-action">
+                        <label for="my-modal-6" class="btn">Enjoy More News</label>
                     </div>
     `
-    modalArea.appendChild(modalDiv)
 }
 
 // Set Loader
